ae81eec377b15c5fff1de10d5bd564d43f06e07e
ammelung先生 <61866239+julius-ammelung@users.noreply.github.com>
2020-03-17 18:28:43 +0100

    Speicher begonnen
    Mit dem Abspeichern der Punktzahl als Cookie wurde begonnen.  Es ist noch unvollständig. Zusätzlich habe ich einiges, was nicht unbedingt zur Datei "speicher.js" gehört aus dieser entfernt; die ursprüngliche Datei findet sich unter dem Namen "OLD_speicher.js"

--- javascript/speicher/speicher.js
+++ javascript/speicher/speicher.js
@@ -1,31 +1,9 @@
-"use strict";
+/*"use strict";*/
 
-function weiter(){
-    if(fragen(spielstand)){
-        spielstand ++;
-    if(spielstand == 10){
-        alert("Spiel beendet");
-        spielstand = 1;
-        }
-    }
-    localStorage.setItem("spielstand",spielstand);
+function writeSavedata(punkte){
+	document.cookie = "MathespielSavedata=" + punkte;
 }
 
-function neu(){
-    spielstand = 1;
-    localStorage.setItem("Spielstand",spielstand);
-}
-
-
-let spielstand = 0;
-
-if (localStorage.getItem("spielstand") == null){
-   spielstand = 1;
-}
-else {
-    spielstand = localStorage.getItem("spielstand");
-}
-
-naechsterzug.onclick = weiter;
-neuesSpiel.onclick = neu;
-
+/*function readSavedata(){
+	;
+}*/
