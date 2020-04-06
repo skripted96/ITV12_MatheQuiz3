e4e38173dc5e3a41e046007ef8f616e420f2b150
frumpymoons <frumpymoons@users.noreply.github.com>
2020-04-06 15:32:58 +0200

    Pseudo privacy

--- javascript/fragen/fragen.js
+++ javascript/fragen/fragen.js
@@ -1,31 +1,31 @@
 // 3 Schwierigkeitsgrade
-// Einfach:	a * b 			Regex: \d+\*\d+
-// Mittel:	a * b -+ c		Regex: \d+\*\d+[-+]\d+
-// Schwer:	a * b -+ c * d	Regex: \d+\*\d+[-+]\d+\*(1[0-9]||20||[2-9])
+// Einfach:	a * b 			Regex: \d+\*\d+$
+// Mittel:	a * b -+ c		Regex: \d+\*\d+[-+]\d+$
+// Schwer:	a * b -+ c * d	Regex: \d+\*\d+[-+]\d+\*(1[0-9]|20|[2-9])$
 // Alle Zahlen werden zufällig generiert
 // Nur ganze Zahlen
 // 1 - 20
 
 class Frage {
 	constructor() {
-		this.frageRegex = [
-			/\d+\*\d+/,
-			/\d+\*\d+[-+]\d+/,
-			/\d+\*\d+[-+]\d+\*(1[0-9]||20||[2-9])/
+		this._frageRegex = [
+			/\d+\*\d+$/,
+			/\d+\*\d+[-+]\d+$/,
+			/\d+\*\d+[-+]\d+\*(1[0-9]|20|[2-9])$/
 		];
 	
-		this.frageString = [
+		this._frageString = [
 			'\d*\d',
 			'\d*\d?\d',
 			'\d*\d?\d*\d'
 		];
 	}
 
-	zufallszahlGenerieren(min, max) {
+	_zufallszahlGenerieren(min, max) {
 		return Math.floor(Math.random() * (max - min + 1) + min);
 	}
 
-	schwierigkeitErmitteln(spielstand) {
+	_schwierigkeitErmitteln(spielstand) {
 		// Position des Spielers ermitteln
 		// Feld 1-3 = Einfach
 		// Feld 4-6 = Mittel
@@ -46,45 +46,44 @@ class Frage {
 		}
 	}
 
-	fragePruefen(frage, schwierigkeit) {
-		if (this.frageRegex[schwierigkeit - 1].test(frage))
+	_fragePruefen(frage, schwierigkeit) {
+		if (this._frageRegex[schwierigkeit - 1].test(frage))
 			return true;
 		return false;
 	}
 
-	ergebnisBerechnen(frage) {
-		var ergebnis = eval(frage);
-		return ergebnis;
+	_ergebnisBerechnen(frage) {
+		return eval(frage);
 	}
 
 	ergebnisPruefen(frage, ergebnis) {
-		if (ergebnis == this.ergebnisBerechnen(frage))
+		if (ergebnis == this._ergebnisBerechnen(frage))
 			return true;
 		return false;
 	}
 	
 	frageGenerieren(spielstand) {
-		let schwierigkeit = this.schwierigkeitErmitteln(spielstand);
-		if (schwierigkeit == 0) { return; }
+		let schwierigkeit = this._schwierigkeitErmitteln(spielstand);
+		if (schwierigkeit == 0)
+			return;
 
-		let frage = this.frageString[schwierigkeit - 1];
+		let frage = this._frageString[schwierigkeit - 1];
 
 		let i = schwierigkeit;
 		do {
-			frage = frage.replace('\d', this.zufallszahlGenerieren(1, 20));
+			frage = frage.replace('\d', this._zufallszahlGenerieren(1, 20));
 			i--;
 		} while (i != -1);
 
 		if (spielstand > 3) {
-			if (this.zufallszahlGenerieren(0, 1) == 1) {
+			if (this._zufallszahlGenerieren(0, 1) == 1) {
 				frage = frage.replace('?', '+');
-			}
-			else {
+			} else {
 				frage = frage.replace('?', '-');
 			}
 		}
 
-		if (this.fragePruefen(frage, schwierigkeit)) {
+		if (this._fragePruefen(frage, schwierigkeit)) {
 			return frage;
 		} else {
 			return this.frageGenerieren(spielstand);
